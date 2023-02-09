c6be702ccd7c12fbf9b98f6914711944a8e10386
KoushikKaleru18 <kalerukoushik18@gmail.com>
2023-02-10 04:17:48 +0530

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function NotFound() {
   return (
     <div className='Container my-5 mx-5'>
       <h2>Page not found!!</h2>
+      <Link to="/" className='btn btn-primary'>Home</Link>
     </div>
   )
 }
