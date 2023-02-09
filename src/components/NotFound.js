73805206f31642a57749d05b2b54cb4fddf6d731
Rick <kalerukoushik@gmail.com>
2023-02-10 03:59:38 +0530

    Added Routing

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='Container my-5 mx-5'>
+      <h2>Page not found!!</h2>
+    </div>
+  )
+}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,44 +5,83 @@ function TextForm(props) {
 
   const handleUpperCase = () => {
     setText(text.trim().toUpperCase());
-  }
+    props.showAlert("Converted to Upper case", "success")
+  };
 
   const handleLowerCase = () => {
-    setText(text.trim().toLowerCase())
-  }
+    setText(text.trim().toLowerCase());
+    props.showAlert("Converted to Lower case", "success")
+  };
+
+  const handleClearText = () => {
+    setText("");
+    props.showAlert("All Clear, Buddy", "info")
+  };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
+  };
+
+  const handleCopyText = () => {
+    var text = document.getElementById('myBox')
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard", "success");
   }
 
   return (
     <>
-    <div className="container my-4">
-      <h3>{props.heading}</h3>
-      <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8" placeholder="Enter your text here"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
-      </div>
-      {/* uppercase */}
-      <button className="btn btn-primary mx-1"
-        onClick={handleUpperCase}
-      > Convert to Uppercase </button>
-      {/* lowercase */}
-      <button className="btn btn-primary mx-1"
-        onClick={handleLowerCase}
-      > Convert to Lowercase </button>
-    </div>
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "light" ? "#343a40" : "white" }}
+      >
+        <h3>{props.heading}</h3>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            placeholder=""
+            value={text}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "#343a40",
+              color: props.mode === "light" ? "#343a40" : "white",
+            }}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        {/* uppercase */}
+        <button className="btn btn-primary mx-1" onClick={handleUpperCase}>
+          Convert to Uppercase
+        </button>
+        {/* lowercase */}
+        <button className="btn btn-primary mx-1" onClick={handleLowerCase}>
+          Convert to Lowercase
+        </button>
 
-    <div className="container my-4">
+        {/* Copy to clipboard */}
+        <button className="btn btn-primary mx-1" onClick={handleCopyText}>
+          Copy to Clipboard
+        </button>
+        {/* Clear text */}
+        <button className="btn btn-danger mx-1" onClick={handleClearText}>
+          Clear
+        </button>
+      </div>
+
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "light" ? "#343a40" : "white" }}
+      >
         <h4>Text summary</h4>
-        <p>This text contains: {text.split(" ").length} words and {text.length} letters</p>
+        <p>
+          This text contains: {text.split(" ").length} words and {text.length}
+          letters
+        </p>
         <p>{text.split(" ").length * 0.008} minutes read</p>
-    </div>
+      </div>
     </>
   );
 }
-
 
 export default TextForm;
